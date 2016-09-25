14deee5fc1cd6e3cf5b8b161558816f1c69b1dd6
ssuyi <elic.space.cs@gmail.com>
2016-09-25 16:26:29 +0800

    netctl

--- setup/arch_base.sh
+++ setup/arch_base.sh
@@ -81,8 +81,8 @@ base_2() {
     locale-gen
     echo LANG=en_US.UTF-8 > /etc/locale.conf
 
-    IF=ens33
-    sed -e 's/eth0/$IF/' /etc/netctl/examples/ethernet-dhcp  > /etc/netctl/dhcp
+    cp /etc/netctl/examples/ethernet-dhcp  > /etc/netctl/dhcp
+    vim /etc/netctl/dhcp
     netctl enable dhcp
 
     pacman --noconfirm -S openssh zsh sudo
